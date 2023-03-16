7912df82c4989a7d62c135691addd7c107748a1c
Dev Fred <manfredtochukwu362@gmail.com>
2023-03-16 15:31:44 +0000

    first commit

---
+++ auth-server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+//middlewares
+app.use(express.json())
+
+//routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+app.listen(process.env.AUTH_PORT || 2000)
